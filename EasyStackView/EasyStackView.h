9c483a779da6c78b08227329d9c0d63f049720a0
Elenion <stellanxu@gmail.com>
2020-04-02 11:04:21 +0800

    Initial Commit

---
+++ EasyStackView/EasyStackView.h
@@ -0,0 +1,19 @@
+//
+//  EasyStackView.h
+//  EasyStackView
+//
+//  Created by Elenion on 2020/4/2.
+//  Copyright © 2020 Elenion. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EasyStackView.
+FOUNDATION_EXPORT double EasyStackViewVersionNumber;
+
+//! Project version string for EasyStackView.
+FOUNDATION_EXPORT const unsigned char EasyStackViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EasyStackView/PublicHeader.h>
+
+
